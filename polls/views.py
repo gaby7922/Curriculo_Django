f27277b2a3de8998ca5df21006a24d9538b7273b
gaby hernandez <gaby7922@gmail.com>
2018-09-26 19:36:00 -0300

    Actualicacion a variables

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,17 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-
 
 def index(request):
     # return HttpResponse("Hello, world. You're at the polls index.")
-    return render(request, 'index.html')
+    data = {}
+    data['personal'] = {"name": "Gaby",
+                        "lastname": "Hernandez"}
+    data['address'] = {"home": "Ramon Laval 1638"}
+
+    data['abilities'] = {"programming_languages":['Python',' Php'],
+                        "marking_language":"JavaScript, Html4, CSS",
+                        "Framework":"Django","Methodologies":" Metodologías ágiles, Scrum", 
+                        "additional":"Analista y Programador de Sistemas Web,Líder de Proyecto", 
+                        "database":"Manejo y Administración de Bases de Datos PostgresSQL,  sql",
+                        "others":"Elaboración de Casos de Uso,Administración y Desarrollo de Plataformas Distribuidas, Experiencia en el area del analisis"}
+    
+    return render(request, 'index.html', data)
